a5b372691693e8b13bd06683e921accc577f0741
IgorMartinsOli <igrmartoli@gmail.com>
2023-07-20 11:20:09 -0300

    Criando estados para pesquisa de livros, modificando estado com onBlur, modificar componentes a partir do estado para que tenhamos componentes dinâmicos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header/index.js';
 import styled from 'styled-components';
+import Pesquisa from './components/Pesquisa/index.js';
 
 const AppContainer = styled.div`
   width: 100vw;
@@ -11,6 +12,7 @@ function App() {
     return (
       <AppContainer> 
         <Header/>
+        <Pesquisa/>
       </AppContainer>
   );
 }
---
+++ src/components/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,45 @@
+import livro from '../../images/livro.png'
+import livro2 from '../../images/livro2.png'
+
+export const livros = [
+    {
+        "id": 1,
+        "nome": "Livro de Java",
+        "src": livro
+    },
+    {
+        "id": 2,
+        "nome": "Livro de Python",
+        "src": livro
+    },
+    {
+        "id": 3,
+        "nome": "Livro de JS",
+        "src": livro
+    },
+    {
+        "id": 4,
+        "nome": "Livro de SQL",
+        "src": livro
+    },
+    {
+        "id": 6,
+        "nome": "Java para aventureiros",
+        "src": livro2
+    },
+    {
+        "id": 7,
+        "nome": "Python para aventureiro",
+        "src": livro2
+    },
+    {
+        "id": 8,
+        "nome": "JS para aventureiro",
+        "src": livro2
+    },
+    {
+        "id": 9,
+        "nome": "SQL para aventureiro",
+        "src": livro2
+    }
+]
